86778dcc9404091a4fb4c67afdeea95b373e8cb5
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-03-28 12:16:47 +0530

    All Done

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-      <footer className="bg-gray-900 text-gray-300 py-12 w-full mt-16">
+      <footer className="bg-gray-900 text-gray-300 py-12 w-full">
         <div className="container mx-auto px-1">
           <div className="grid grid-cols-1 md:grid-cols-4 gap-8 ">
             
